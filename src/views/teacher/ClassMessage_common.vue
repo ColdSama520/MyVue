7527be4a5e705f12480fcccd7bfe59ba105525c9
ColdSama520 <1360614758@qq.com>
2021-12-26 22:34:45 +0800

    正常班课over

--- src/views/teacher/ClassMessage_common.vue
+++ src/views/teacher/ClassMessage_common.vue
@@ -10,33 +10,45 @@
       </el-breadcrumb>
     </div>
     <div class="container">
-      <el-table>
+      <div class="handle-box">
+        <el-input v-model="searchTable.course_id" placeholder="课程ID" class="handle-input mr10"></el-input>
+        <el-button type="primary" icon="el-icon-search" @click="handleSearch()">搜索</el-button>
+      </div>
+      <el-table :data="teacher.teacherData" border class="teacher" ref="multipleTable" header-cell-class-name="table-header">
         <el-table-column prop="teacher_id" label="教师ID"></el-table-column>
         <el-table-column prop="teacher_name" label="教师姓名"></el-table-column>
         <el-table-column prop="teacher_phone" label="联系方式"></el-table-column>
       </el-table>
-      <el-table :data="tableData" border class="table" ref="multipleTable" header-cell-class-name="table-header">
+      <el-table :data="course.courseData" border class="course" ref="multipleTable" header-cell-class-name="table-header">
         <el-table-column prop="course_id" label="课程ID"></el-table-column>
         <el-table-column prop="course_name" label="课程名"></el-table-column>
         <el-table-column prop="course_start_year" label="开课日期"></el-table-column>
         <el-table-column prop="course_is_archive" label="是否归档">
           <template #default="scope">
             <el-tag :type="
-                                scope.row.state === '成功'
+                               scope.row.course_is_archive === '是'
                                     ? 'success'
-                                    : scope.row.state === '失败'
+                                    : scope.row.course_is_archive === '否'
                                     ? 'danger'
                                     : ''
-                            ">归档</el-tag>
+                            ">{{scope.row.course_is_archive}}</el-tag>
           </template>
         </el-table-column>
         <el-table-column prop="student_class" label="上课班级"></el-table-column>
-        <el-table-column prop="date" label="上课人数"></el-table-column>
+        <el-table-column prop="student_number" label="上课人数"></el-table-column>
         <el-table-column label="操作" width="180" align="center">
           <template #default="scope">
-            <el-button type="text" icon="el-icon-edit" @click="handleEdit(scope.$index, scope.row)">编辑
-            </el-button>
-            <el-button type="text" icon="el-icon-delete" class="red" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+            <el-switch
+                style="display: block"
+                v-model="scope.row.course_is_archive"
+                active-color="#ff4949"
+                inactive-color="#13ce66"
+                active-text="归档"
+                inactive-text="正常"
+                active-value="是"
+                inactive-value="否"
+                @change="updateArchive(scope.row.course_id)">
+            </el-switch>
           </template>
         </el-table-column>
       </el-table>
@@ -45,22 +57,6 @@
         <el-button type="primary" @click="handleRead">添加班课</el-button>
       </div>
     </div>
-      <el-dialog title="编辑" v-model="editVisible" width="30%">
-          <el-form label-width="70px">
-              <el-form-item label="用户名">
-                  <el-input v-model="form.name"></el-input>
-              </el-form-item>
-              <el-form-item label="地址">
-                  <el-input v-model="form.address"></el-input>
-              </el-form-item>
-          </el-form>
-          <template #footer>
-                <span class="dialog-footer">
-                    <el-button @click="editVisible = false">取 消</el-button>
-                    <el-button type="primary" @click="saveEdit">确 定</el-button>
-                </span>
-          </template>
-      </el-dialog>
 
 
   </div>
@@ -70,6 +66,9 @@
 import { ref, reactive } from "vue";
 import { ElMessage, ElMessageBox } from "element-plus";
 import { fetchData } from "../../api";
+import axios from "axios";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
 
 export default {
       name: "classmessage_common",
@@ -79,61 +78,116 @@ export default {
         }
       },
       setup() {
-        const query = reactive({
-          name: "",
+
+        const router = useRouter();
+
+        const teacher = reactive({
+          teacherData: []
+        })
+        const course = reactive({
+          courseData: []
         });
-        const tableData = ref([]);
+        const tableData = reactive({
+          teacher_id: localStorage.getItem("ms_username"),
+        })
+
         // 获取表格数据
         const getData = () => {
-          fetchData(query).then((res) => {
-            tableData.value = res.list;
-          });
+          axios.get('http://localhost:9090/Teacher/search', { params : tableData })
+              //成功返回
+              .then(response => {
+                console.log(response);
+                if(response.status === 200) {
+                  teacher.teacherData = response.data;
+                }
+                else{
+                  return false;
+                }
+              })
+              //失败返回
+              .catch(error => {
+                console.log(error);
+                return false;
+              })
+          axios.get('http://localhost:9090/Course/CourseCommon', { params : tableData })
+              //成功返回
+              .then(response => {
+                console.log(response);
+                if(response.status === 200) {
+                  course.courseData = response.data;
+                }
+                else{
+                  return false;
+                }
+              })
+              //失败返回
+              .catch(error => {
+                console.log(error);
+                return false;
+              })
         };
         getData();
 
-      // 删除操作
-      const handleDelete = (index) => {
-          // 二次确认删除
-          ElMessageBox.confirm("确定要删除吗？", "提示", {
-              type: "warning",
-          })
-              .then(() => {
-                  ElMessage.success("删除成功");
-                  tableData.value.splice(index, 1);
+        const store = useStore();
+        const updateArchive = (id) => {
+          axios.get('http://localhost:9090/Course/updateUnCommon', { params : {course_id: id} })
+              //成功返回
+              .then(response => {
+                console.log(response);
+                if(response.status === 200) {
+                  ElMessage.success("修改成功");
+                  store.commit("clearTags");
+                  router.push('/classmessage_uncommon')
+                }
+                else{
+                  return false;
+                }
               })
-              .catch(() => {});
-      };
+              //失败返回
+              .catch(error => {
+                console.log(error);
+                return false;
+              })
+        }
 
-      // 表格编辑时弹窗和保存
-      const editVisible = ref(false);
-      let form = reactive({
-          name: "",
-          address: "",
-      });
-      let idx = -1;
-      const handleEdit = (index, row) => {
-          idx = index;
-          Object.keys(form).forEach((item) => {
-              form[item] = row[item];
-          });
-          editVisible.value = true;
-      };
-      const saveEdit = () => {
-          editVisible.value = false;
-          ElMessage.success(`修改第 ${idx + 1} 行成功`);
-          Object.keys(form).forEach((item) => {
-              tableData.value[idx][item] = form[item];
-          });
-      };
+        const searchTable = reactive({
+          course_id: '',
+          teacher_id: tableData.teacher_id,
+        })
+
+        const handleSearch = () => {
+          if(searchTable.course_id === '')
+            getData();
+          else {
+            axios.get('http://localhost:9090/Course/CourseCommonMessage', {params: searchTable})
+                //成功返回
+                .then(response => {
+                  console.log(response);
+                  if (response.status === 200) {
+                    if (response.data.length === 0) {
+                      ElMessage.error("请输入该教师所教授科目ID");
+                    }else{
+                      course.courseData = response.data;
+                    }
+                  } else {
+                    return false;
+                  }
+                })
+                //失败返回
+                .catch(error => {
+                  console.log(error);
+                  return false;
+                })
+          }
+        };
 
     return {
-        query,
+        teacher,
+        course,
         tableData,
-        form,
-        editVisible,
-        handleDelete,
-        handleEdit,
-        saveEdit,
+        searchTable,
+        updateArchive,
+        handleSearch,
     };
   },
 };
@@ -152,7 +206,11 @@ export default {
   width: 300px;
   display: inline-block;
 }
-.table {
+.teacher {
+  width: 100%;
+  font-size: 14px;
+}
+.course {
   width: 100%;
   font-size: 14px;
 }
