91ea69c6b3dfa5c380e6e7f423d6c6181e9f7a0b
ColdSama520 <1360614758@qq.com>
2021-12-27 12:23:34 +0800

    添加班课half

--- src/views/teacher/ClassMessage_common_add.vue
+++ src/views/teacher/ClassMessage_common_add.vue
@@ -10,15 +10,15 @@
         </div>
         <div class="container">
             <div class="form-box">
-                <el-form ref="formRef" :rules="rules" :model="form" label-width="80px">
-                    <el-form-item label="课程名称" prop="name">
-                      <el-input v-model="form.name"></el-input>
+                <el-form ref="formRef" :rules="rules" :model="course" label-width="80px">
+                    <el-form-item label="课程ID" prop="course_id">
+                      <el-input v-model="course.course_id"></el-input>
                     </el-form-item>
-                  <el-form-item label="开课年份" prop="date">
-                    <el-input v-model="form.date"></el-input>
-                  </el-form-item>
-                  <el-form-item label="学生班级" prop="class">
-                    <el-input v-model="form.class"></el-input>
+                    <el-form-item label="课程名称" prop="course_name">
+                      <el-input v-model="course.course_name"></el-input>
+                    </el-form-item>
+                  <el-form-item label="开课年份" prop="course_start_year">
+                    <el-input v-model="course.course_start_year"></el-input>
                   </el-form-item>
                     <el-form-item>
                         <el-button type="primary" @click="onSubmit">提交</el-button>
@@ -34,33 +34,66 @@
 <script>
 import { reactive, ref } from "vue";
 import { ElMessage } from "element-plus";
+import axios from "axios";
+import { useRouter } from "vue-router";
 export default {
     name: "classmessage_common_add",
     setup() {
+        const router = useRouter();
+
         const rules = {
-            name: [
+            course_id: [
+              {
+                required: true, message: "请输入班课ID", trigger: "blur"
+              }
+            ],
+            course_name: [
             { required: true, message: "请输入班课名称", trigger: "blur" },
             ],
-            date: [
+            course_start_year: [
               { required: true, message: "请输入开课日期", trigger: "blur" },
             ],
-            class: [
-            { required: true, message: "请输入开课班级", trigger: "blur" },
-          ],
         };
         const formRef = ref(null);
-        const form = reactive({
-            name: "",
-            date: "",
-            class: "",
+        const course = reactive({
+          course_id: "",
+          course_name: "",
+          course_start_year: "",
+          course_is_archive: "否",
+          teacher_id: localStorage.getItem("ms_username"),
         });
         // 提交
         const onSubmit = () => {
+            let flag = 1;
             // 表单校验
             formRef.value.validate((valid) => {
                 if (valid) {
-                    console.log(form);
-                    ElMessage.success("提交成功！");
+                  axios.get('http://localhost:9090/Course/IsHasCourse', { params : course })
+                      //成功返回
+                      .then(response => {
+                        console.log(response);
+                        if(response.status === 200) {
+                          if(response.data.length === 1) {
+                            flag = 0;
+                            ElMessage.error("请填写正确课程ID");
+                            formRef.value.resetFields();
+                          }
+                        }
+                        else{
+                          return false;
+                        }
+                      })
+                  axios.get('http://localhost:9090/Course/addCourseCommon', {params: course})
+                      //成功返回
+                      .then(response => {
+                        console.log(response);
+                        if (response.status === 200) {
+                          ElMessage.success("添加课程成功！请输入或导入学生！");
+                          router.push("/");
+                        } else {
+                          return false;
+                        }
+                      })
                 } else {
                     return false;
                 }
@@ -74,7 +107,7 @@ export default {
         return {
             rules,
             formRef,
-            form,
+            course,
             onSubmit,
             onReset,
         };
